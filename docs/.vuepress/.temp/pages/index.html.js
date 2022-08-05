8423cb3dfba23a139f1034d2fee229e0090dbb45
unknown <gmcabrera121190@gmail.com>
2022-08-05 17:26:43 -0300

    Adding documentation.

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"v3-scroll\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":3,\"title\":\"Installation\",\"slug\":\"installation\",\"children\":[]},{\"level\":3,\"title\":\"Props\",\"slug\":\"props\",\"children\":[]}],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"v3-scroll\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":3,\"title\":\"Installation\",\"slug\":\"installation\",\"children\":[]},{\"level\":3,\"title\":\"Props\",\"slug\":\"props\",\"children\":[]}],\"git\":{\"updatedTime\":1659728508000,\"contributors\":[{\"name\":\"unknown\",\"email\":\"gmcabrera121190@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
